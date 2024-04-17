99b842638661f0a8cfa50d1112d024d8faaf8438
YakultGo <god1239426256@163.com>
2024-04-17 14:30:53 +0800

    完成取号逻辑

---
+++ sequence/mysql.go
@@ -0,0 +1,43 @@
+package sequence
+
+import (
+	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
+
+// 建立mysql连接，执行replace info语句
+// REPLACE INFO sequence(stub) VALUES('a');
+// SELECT LAST_INSERT_ID();
+const (
+	sqlReplaceInfoStub = `REPLACE INFO sequence(stub) VALUES('a');`
+)
+
+type MySQL struct {
+	conn sqlx.SqlConn
+}
+
+func NewMySQL(dsn string) *MySQL {
+	return &MySQL{
+		conn: sqlx.NewMysql(dsn),
+	}
+}
+
+func (m *MySQL) Next() (uint64, error) {
+	stmt, err := m.conn.Prepare(sqlReplaceInfoStub)
+	if err != nil {
+		logx.Errorw("conn.Prepare failed", logx.LogField{Key: "err", Value: err.Error()})
+		return 0, err
+	}
+	defer stmt.Close()
+	rest, err := stmt.Exec()
+	if err != nil {
+		logx.Errorw("stmt.Exec failed", logx.LogField{Key: "err", Value: err.Error()})
+		return 0, err
+	}
+	id, err := rest.LastInsertId()
+	if err != nil {
+		logx.Errorw("rest.LastInsertId failed", logx.LogField{Key: "err", Value: err.Error()})
+		return 0, err
+	}
+	return uint64(id), nil
+}
